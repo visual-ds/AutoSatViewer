e32643cf15e129908f54b63c9cc1c625ce30bfb0
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-08-08 10:05:08 -0300

    changed serving files to work properly

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, json,jsonify
+from flask import Flask, render_template, request, json,jsonify, send_from_directory
 import csv
 import pandas as pd
 import numpy as np
@@ -7,7 +7,8 @@ import os
 from glob import glob
 
 app = Flask(__name__,static_url_path='/static')
-files = glob('static/data/*.png')
+#files = glob('static/data/*.png')
+files = glob("/media/giovani/DATA/gee-data/patches/image_18/*.jpg")
 
 @app.route("/",methods=["GET","POST"])
 def index():
@@ -16,7 +17,10 @@ def index():
 
 @app.route("/tiles/<int:z>/<int:x>/<int:y>.png")
 def tiles(z,x,y):
-    tile = files[1500]
+    tile = [f for f in files if f"{x}_{y}_{z}" in f]
+    if len(tile) == 0:
+        return '', 204
+    tile = tile[0]
     tile = open(tile, 'rb').read()
     return tile, 200, {'Content-Type': 'image/png'}
 
