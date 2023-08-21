4d064d8402f843698608875eab9480e7e78ee549
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-08-21 14:10:16 -0300

    update function to serve tiles

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, json,jsonify, send_from_directory
+from flask import Flask, render_template, request, json, jsonify, send_from_directory
 import csv
 import pandas as pd
 import numpy as np
@@ -6,24 +6,24 @@ import math
 import os
 from glob import glob
 
-app = Flask(__name__,static_url_path='/static')
-#files = glob('static/data/*.png')
-files = glob("/media/giovani/DATA/gee-data/patches/image_18/*.jpg")
+app = Flask(__name__, static_url_path="/static")
 
-@app.route("/",methods=["GET","POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     print(os.path.dirname(app.instance_path))
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.route("/tiles/<int:z>/<int:x>/<int:y>.png")
-def tiles(z,x,y):
-    tile = [f for f in files if f"{x}_{y}_{z}" in f]
-    if len(tile) == 0:
-        return '', 204
-    tile = tile[0]
-    tile = open(tile, 'rb').read()
-    return tile, 200, {'Content-Type': 'image/png'}
+def tiles(z, x, y):
+    tile = f"/media/giovani/DATA/gee-data/patches/image_0/patch_{x}_{y}_{z}.jpg"
+    try:
+        tile = open(tile, "rb").read()
+        return tile, 200, {"Content-Type": "image/png"}
+    except:
+        return "", 204
 
 
-if __name__=="__main__":
-	app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
