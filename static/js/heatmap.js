dd3c61f8b3c2eb8da600ae27fe5177883bfa1293
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-09-24 16:48:06 -0300

    small changes to heatmap

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -2,9 +2,8 @@ const total_width = document.getElementById("timeslider").clientWidth;
 const margin = {top: 5, right: 30, bottom: 5, left: 30};
 const width = total_width - margin.left - margin.right;
 const height = 120 - margin.top - margin.bottom;
-const VALUE = "spatiotemporal_coeff_1"
+const VALUE = "spatiotemporal_torque"
 
-// append the svg object to the body of the page
 const svg = d3
     .select('div#temporalChartLine')
     .append('svg')
@@ -14,7 +13,6 @@ const svg = d3
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-// Build X scales and axis:
 var x = d3.scaleBand()
   .range([ 0, width ])
   .domain(Array(1024).fill().map((_, i) => i))
@@ -23,20 +21,16 @@ svg.append("g")
   .attr("transform", "translate(0," + height + ")")
   .call(d3.axisBottom(x))
 
-// Build X scales and axis:
-var y = d3.scaleBand()
-  .range([ height, 0 ])
-  .domain(Array(20).fill().map((_, i) => i))
-  .padding(0.01);
-svg.append("g")
-   .call(d3.axisLeft(y));
 
-
-
-// load csv from flask static folder
-d3.csv("/static/data/data.csv", d3.autoType)
+d3.csv("/static/data/data_diff_half.csv", d3.autoType)
     .then(function(data) {
-
+        var y = d3.scaleBand()
+            .range([ height, 0 ])
+            .domain([...new Set(data.map(d => d.t))]) 
+            .padding(0.01);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+        
         var tooltip = d3.select("div#temporalChartLine")
             .append("div")
             .style("opacity", 0)
@@ -51,23 +45,17 @@ d3.csv("/static/data/data.csv", d3.autoType)
 
             // Three function that change the tooltip when user hover / move / leave a cell
             var mouseover = function(d) {
-                d3.select(this)
-                    .style("stroke", "black")
-                    .attr("width", x.bandwidth() * 5)
-                    .attr("height", y.bandwidth() *5);
+                d3.select(this).style("stroke", "black");
                 tooltip.style("opacity", 1)
             }
             var mousemove = function(event, d) {
             tooltip
-                .html("The exact value of<br>this cell is: " + d[VALUE])
+                .html("Torque:" + Math.round(d[VALUE] * 100) / 100 + "<br/>" + "Timestamp:" + Math.round(d.t))
                 .style("left", (d3.pointer(event)[0] + 40)+ "px")
                 .style("top", (d3.pointer(event)[1] + 40) + "px")
             }
             var mouseleave = function(d) {
-                d3.selectAll(".cell")
-                .style("stroke", "none")
-                .attr("width", x.bandwidth())
-                .attr("height", y.bandwidth());
+                d3.selectAll(".cell").style("stroke", "none");
             tooltip.style("opacity", 0)
             }
         
@@ -89,6 +77,7 @@ d3.csv("/static/data/data.csv", d3.autoType)
             .on("mousemove", mousemove)
             .on("mouseleave", mouseleave)
             .on("click", function(event, d) {
-                map.setView(new L.LatLng(d.lat, d.lon), 12);
+                map.setView(new L.LatLng(d.lat, d.lon), 9);
+                sliderTime.value(d.t * 2);
             })
     });
