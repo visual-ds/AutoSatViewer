4d45f3a4be12387054fe347393b5c618a69dd4d7
germaingarcia <germaingarcia17@gmail.com>
2023-11-16 00:06:40 -0500

    maps modification

--- static/js/drag.js
+++ static/js/drag.js
@@ -1,4 +1,4 @@
-var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+/*var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 var divDraggable = document.getElementById("draggable"); // div principal
 var dragBtn = document.getElementById("drag-btn"); // boton usado para mover 
 
@@ -23,4 +23,49 @@ function elementDrag(e) {
 function closeDragElement() {
   document.onmouseup = null;
   document.onmousemove = null;
+}*/
+
+const leftPanel = document.getElementById('mapa-left');
+const centerPanel = document.getElementById('map');
+const rightPanel = document.getElementById('mapa-right');
+const resizerLeft = document.getElementById('resizer-left');
+const resizerRight = document.getElementById('resizer-right');
+
+let isResizingLeft = false;
+let isResizingRight = false;
+
+resizerLeft.addEventListener('mousedown', function(e) {
+    isResizingLeft = true;
+    document.addEventListener('mousemove', handleResizeLeft);
+    document.addEventListener('mouseup', stopResizeLeft);
+});
+
+resizerRight.addEventListener('mousedown', function(e) {
+    isResizingRight = true;
+    document.addEventListener('mousemove', handleResizeRight);
+    document.addEventListener('mouseup', stopResizeRight);
+});
+
+function handleResizeLeft(e) {
+    if (isResizingLeft) {
+        const newWidth = e.clientX - leftPanel.getBoundingClientRect().left;
+        leftPanel.style.width = newWidth + 'px';
+    }
+}
+
+function handleResizeRight(e) {
+    if (isResizingRight) {
+        const newWidth = rightPanel.getBoundingClientRect().right - e.clientX;
+        rightPanel.style.width = newWidth + 'px';
+    }
+}
+
+function stopResizeLeft() {
+    isResizingLeft = false;
+    document.removeEventListener('mousemove', handleResizeLeft);
+}
+
+function stopResizeRight() {
+    isResizingRight = false;
+    document.removeEventListener('mousemove', handleResizeRight);
 }
