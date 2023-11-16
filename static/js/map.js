30ef5686fdd760497a95b955efe92cec33d27288
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-11-16 15:44:32 -0300

    clean code

--- static/js/map.js
+++ static/js/map.js
@@ -2,37 +2,9 @@ var T = 0;
 var center_lat = -1;
 var center_lon = -50;
 
-// var cdblight = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-//             attribution: 'Colaboradores: <a href="http://visualdslab.com/">Visual Data Science lab</a> & <a href="https://portal.fgv.br/">FGV</a> |', //+' &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-//             subdomains: 'abcd',
-//             minZoom: 9,
-//             maxZoom: 12,
-//             //maxNativeZoom: 28,
-            
-// });
-
-// var planet = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{ //'https://tileserver-mapbiomas.sccon.com.br/mapbiomas/tiles/1.0.0/planet_mosaic_2021_07/planet/{z}/{x}/{y}.png'
-//             attribution: "",
-//             subdomains: 'abcd',
-//             minZoom: 9,
-//             maxZoom: 12,
-//             //maxNativeZoom: 28, 
-// });
-
-
-// var cdblight2 = L.tileLayer('http://0.gusc.cartocdn.com/cemdevops/api/v1/map/7dc8c60e6ce81b15ab93873c65b74c8e:1527080869781/0/{z}/{x}/{y}.png', {
-//             attribution: 'Colaboradores: <a href="http://visualdslab.com/">Visual Data Science lab</a> & <a href="https://portal.fgv.br/">FGV</a> |', //+' &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-//             subdomains: 'abcd',
-//             minZoom: 9,
-//             maxZoom: 12,
-//            // maxNativeZoom: 28,
-            
-// });
-
-
-const map = L.map('map').setView([-1, -50], 12);
-const map1 = L.map('mapa-left').setView([-1, -50], 12);
-const map2 = L.map('mapa-right').setView([-1, -50], 12);
+// const map = L.map('map').setView([-1, -50], 12);
+// const map1 = L.map('mapa-left').setView([-1, -50], 12);
+// const map2 = L.map('mapa-right').setView([-1, -50], 12);
 
 var tiles = L.tileLayer("http://127.0.0.1:5000/tiles/{z}/{x}/{y}/{time}.png", {
     minZoom: 9,
@@ -54,14 +26,6 @@ var tiles2 = L.tileLayer("http://127.0.0.1:5000/tiles/{z}/{x}/{y}/{time}.png", {
 }).addTo(map2);
 
 
-
-
-//var baseMaps ={
-//  "Carto BD": cdblight,
-//  "Planet": planet
-//}
-
-
 var overlayMaps = {
   "Estados": L.tileLayer.wms('https://production.alerta.mapbiomas.org/geoserver/ows', {layers: "mapbiomas-alertas:dashboard_states-static-layer", transparent: true, format: "image/png", minZoom: 9, maxZoom: 12}),
   "Municipios": L.tileLayer.wms('https://production.alerta.mapbiomas.org/geoserver/ows', {layers: "mapbiomas-alertas:dashboard_cities-static-layer", transparent: true, format: "image/png", minZoom: 9, maxZoom: 12}),
@@ -79,13 +43,11 @@ var scale = L.control.scale({position:'topleft'}); // Creating scale control
 scale.addTo(map); // Adding scale control to the map
 
 
-//var bounds = new L.LatLngBounds({ lat: -23.1, lng: -46.5 }, { lat: -23.387136, lng: -46.410709 });
-//map.setMaxBounds(bounds);
 // Adding some controls
 L.drawLocal.draw.toolbar.buttons.polygon = 'Desenhar região!';
 L.drawLocal.draw.toolbar.buttons.marker  = 'Marcar um ponto!';
 
-//L.control.zoom({ position: 'bottomright'}).addTo(map);
+
 var drawPluginOptions = {
     position: 'topleft',
     draw: {
