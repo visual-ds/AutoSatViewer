4644daf0de7aad71cc4eb2611851c064daec8ad3
germaingarcia <germaingarcia17@gmail.com>
2024-02-15 11:11:42 -0500

    New Version

--- app.py
+++ app.py
@@ -1,67 +1,12 @@
-from flask import Flask, render_template, request, json, jsonify, send_from_directory
-import csv
+from flask import Flask, render_template
 import pandas as pd
-import numpy as np
-import math
-import os
-from glob import glob
 
-app = Flask(__name__, static_url_path="/static")
+app = Flask(__name__)
 
-
-@app.route("/", methods=["GET", "POST"])
+@app.route('/')
 def index():
-    print(os.path.dirname(app.instance_path))
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/tiles/<int:z>/<int:x>/<int:y>/<int:T>.png")
-def tiles(z, x, y, T):
-    load_from_github = True
-    if load_from_github:
-        import requests
-
-        tile = f"https://raw.githubusercontent.com/visual-ds/AutoSatViewer/main/static/tiles/image_0/patch_{x}_{y}_{z}.jpg"
-        try:
-            tile = requests.get(tile).content
-            return tile, 200, {"Content-Type": "image/png"}
-        except:
-            return "", 204
-    else:
-        tile = f"/media/giovani/DATA/gee-data/patches/image_{T}/patch_{x}_{y}_{z}.jpg"
-        try:
-            tile = open(tile, "rb").read()
-            return tile, 200, {"Content-Type": "image/png"}
-        except:
-            return "", 204
-
-
-@app.route("/data_source/<string:source>")
-def load_data_source(source):
-    df = pd.read_csv(
-        "/home/giovani/Documents/amazonia_project/data/vis_tool/data_diff.csv"
-    )
-    df_overview = pd.read_csv(
-        "/home/giovani/Documents/amazonia_project/data/vis_tool/data_diff_overview.csv"
-    )
-    df = df[["t", "pos", "lon_min", "lon_max", "lat_min", "lat_max", source]]
-    df_overview = df_overview[
-        ["t", "pos", "lon_min", "lon_max", "lat_min", "lat_max", source]
-    ]
-    df = df.to_json(orient="records")
-    df_overview = df_overview.to_json(orient="records")
-    return jsonify(data=df, data_overview=df_overview)
-
-
-@app.route("/temporal_graph")
-def load_temporal_graph():
-    res = json.load(
-        open(
-            "/home/giovani/Documents/amazonia_project/data/vis_tool/data_temporal_graph.json"
-        )
-    )
-    return jsonify(res)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
--- static/js/map.js
+++ static/js/map.js
@@ -1,114 +1,13 @@
-var T = 0;
-var center_lat = -1;
-var center_lon = -50;
 
-const map = L.map('map').setView([-1, -50], 12);
-const map1 = L.map('mapa-left').setView([-1, -50], 12);
-const map2 = L.map('mapa-right').setView([-1, -50], 12);
+var map = L.map('map').setView([-15.75, -47.95], 4); // Brazil
 
-var tiles = L.tileLayer("http://127.0.0.1:5000/tiles/{z}/{x}/{y}/{time}.png", {
-    minZoom: 9,
-    maxZoom: 12,
-    attribution: 'Colaboradores: <a href="http://visualdslab.com/">Visual Data Science lab</a> & <a href="https://portal.fgv.br/">FGV</a> |',
-    time: () => T,
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    maxZoom: 18
 }).addTo(map);
-
-var tiles1 = L.tileLayer("http://127.0.0.1:5000/tiles/{z}/{x}/{y}/{time}.png", {
-  minZoom: 9,
-  maxZoom: 12,
-  time: () => Math.max(T - 1, 0),
-}).addTo(map1);
-
-var tiles2 = L.tileLayer("http://127.0.0.1:5000/tiles/{z}/{x}/{y}/{time}.png", {
-  minZoom: 9,
-  maxZoom: 12,
-  time: () => Math.min(T + 1, 19),
-}).addTo(map2);
-
-
-var overlayMaps = {
-  "Estados": L.tileLayer.wms('https://production.alerta.mapbiomas.org/geoserver/ows', {layers: "mapbiomas-alertas:dashboard_states-static-layer", transparent: true, format: "image/png", minZoom: 9, maxZoom: 12}),
-  "Municipios": L.tileLayer.wms('https://production.alerta.mapbiomas.org/geoserver/ows', {layers: "mapbiomas-alertas:dashboard_cities-static-layer", transparent: true, format: "image/png", minZoom: 9, maxZoom: 12}),
-  "Biomes": L.tileLayer.wms('https://production.alerta.mapbiomas.org/geoserver/ows', {layers: "mapbiomas-alertas:dashboard_biomes-static-layer", transparent: true, format: "image/png", minZoom: 9, maxZoom: 12}),
-  "Amazonia Legal": L.tileLayer.wms('https://production.alerta.mapbiomas.org/geoserver/ows', {layers: "mapbiomas-alertas:dashboard_amazonia-legal-static-layer", transparent: true, format: "image/png", minZoom: 9, maxZoom: 12}),
-  "conservation_units": L.tileLayer.wms('https://production.alerta.mapbiomas.org/geoserver/ows', {layers: "mapbiomas-alertas:dashboard_conservation-unit-static-layer", transparent: true, format: "image/png", minZoom: 9, maxZoom: 12}),
-};
-
-
-// Initialise the FeatureGroup to store editable layers
-var drawnItems = new L.FeatureGroup();
-map.addLayer(drawnItems);
-
-var scale = L.control.scale({position:'topleft'}); // Creating scale control
-scale.addTo(map); // Adding scale control to the map
-
-
-// Adding some controls
-L.drawLocal.draw.toolbar.buttons.polygon = 'Desenhar região!';
-L.drawLocal.draw.toolbar.buttons.marker  = 'Marcar um ponto!';
-
-var drawPluginOptions = {
-    position: 'topleft',
-    draw: {
-      polyline: true,//true,
-      marker: true,
-      circle: true,
-      rectangle: false,
-      circlemarker: true,
-      polygon: {
-          allowIntersection: false,
-          drawError: {
-            color: '#e1e100',
-        },
-          shapeOptions: {
-            color: '#bada55'
-          }
-      },
-      editable: false,
-      },
-  };
-  
-  var drawControl = new L.Control.Draw(drawPluginOptions);
-  map.addControl(drawControl);
-
-
-  function positionTooltip(x, y) {
-    tooltip.style("top", y + 10 + "px")
-      .style("left", x + 10 + "px");
-  }
-
-
 /************************************************** TIME SLIDER *********************** */
 var slider_width = document.getElementById("timeslider").clientWidth * 0.99;
-/*
-var slider_width = document.getElementById("timeslider").clientWidth * 0.95;
-var timesteps = 20;
-
-var sliderTime = d3
-    .sliderBottom()
-    .min(1)
-    .max(timesteps)
-    .step(1)
-    .width(slider_width)
-    .displayValue(false)
-    .default(1) 
-    .handle(d3.symbol().type(d3.symbolCircle).size(200)())
-    .on('onchange', (d) => {
-      T = d - 1;
-      tiles.redraw();
-      tiles1.redraw();
-      tiles2.redraw();
-      d3.select('p#value-time').text(d);
-    });
-   
-var gTime = d3.select('div#timeslider')
-  .append('svg')
-  .attr('height', 40)
-  .attr('width', slider_width)
-  .append('g')
-  .attr('transform', 'translate(10, 20)');
-
-gTime.call(sliderTime);*/
 $('#timeslider')
 .width(slider_width)
 .offset({left: 50 - 10, bottom:-20});
@@ -124,52 +23,9 @@ onChange: function(newRange){
   d = newRange['from'];
   T = d - 1;
   console.log(T);
-  tiles.redraw();
-  tiles1.redraw();
-  tiles2.redraw();
+  //tiles.redraw();
+  //tiles1.redraw();
+  //tiles2.redraw();
 }
 });
 /************************************************** TIME SLIDER *********************** */
-
-var layerControl = L.control.layers(
-  null, 
-  overlayMaps,
-  {position: 'topleft'}
-).addTo(map);
-
-/*
-map.sync(map1);
-map.sync(map2);
-map1.sync(map2);
-map1.sync(map);
-map2.sync(map1);
-map2.sync(map);*/
-// Función para sincronizar los mapas
-function syncMaps(mainMap, maps) {
-  mainMap.on('move', () => {
-      maps.forEach(map => map.setView(mainMap.getCenter(), mainMap.getZoom()));
-  });
-}
-
-// Sincronizar los mapas entre sí
-syncMaps(map, [map1, map2]);
-//syncMaps(map1, [map, map2]);
-//syncMaps(map2, [map, map1]);
-
-// Inicializar Split.js para ajuste de ancho de paneles
-Split(['#mapa-left', '#map', '#mapa-right'], {
-  direction: 'horizontal',
-  gutterSize: 8,
-  cursor: 'col-resize',
-  onDragEnd: () => [map,map1, map2].forEach(map => map.invalidateSize()), // Ajustar el tamaño del mapa después del cambio de ancho
-});
-
-
-map.on("moveend", function(e) {
-  center_lat = map.getCenter().lat;
-  center_lon = map.getCenter().lng;
-});
-
-
-
-  
