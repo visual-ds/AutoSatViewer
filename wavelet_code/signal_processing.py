8e97442b2830d66a618d6ea9ac251b786b561b8a
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2024-02-26 13:18:39 -0300

    recomptue wavelet

--- wavelet_code/signal_processing.py
+++ wavelet_code/signal_processing.py
@@ -9,117 +9,6 @@ import networkx as nx
 from wavelet_transform import *
 
 
-def cluster_dominant_feature(
-    coefficients, threshold_method="quantile", threshold_value=0.85, normalize=False
-):
-    """
-    Identify the dominant scale from the Wavelet coefficients.
-    Set a threshold based on the observed values to identify vectors without dominant scale.
-
-    Inputs:
-        coefficients: [n, n_timesteps, n_s] array Wavelet coefficients
-        threshold_quantile: float Quantile to set the threshold
-        normalize - if should normalize each scale of coefficients
-
-    Outputs:
-        dominant_scale: [n, n_timesteps] array Dominant scale
-    """
-    coefficients = coefficients.copy()
-    coefficients = np.abs(coefficients)
-    n_s = coefficients.shape[2]
-
-    if normalize:
-        for i in range(coefficients.shape[2]):
-            coeffs_max = coefficients[:, :, i].max()
-            coeffs_min = coefficients[:, :, i].min()
-            coefficients[:, :, i] = (coefficients[:, :, i] - coeffs_min) / (
-                coeffs_max - coeffs_min
-            )
-
-    if threshold_method == "quantile":
-        for i in range(coefficients.shape[2]):
-            threshold = np.quantile(coefficients[:, :, i], threshold_value)
-            coefficients[:, :, i][coefficients[:, :, i] < threshold] = 0
-    elif threshold_method == "value":
-        for i in range(coefficients.shape[2]):
-            coefficients[:, :, i][coefficients[:, :, i] < threshold_value[i]] = 0
-    else:
-        raise ValueError("Unknown threshold method")
-
-    labels = np.zeros((coefficients.shape[0], coefficients.shape[1]))
-    for i in range(coefficients.shape[0]):
-        for j in range(coefficients.shape[1]):
-            dominant_idx = np.where(coefficients[i, j, :] > 0)[0]
-            # if there is no dominant, skip
-            if len(dominant_idx) == 0:
-                continue
-            # if there is only one dominant, take it
-            elif len(dominant_idx) == 1:
-                labels[i, j] = dominant_idx[0] + 1
-            # if there is multiple dominant, check if the distance between them is at max 1
-            elif max(np.diff(dominant_idx)) > 1:
-                continue
-            # otherwise take the dominant with the highest value"
-            else:
-                labels[i, j] = np.argmax(coefficients[i, j, :]) + 1
-
-    labels_fixed = labels.copy()
-    labels_fixed[labels == 1] = -2
-    labels_fixed[labels == 2] = -1
-    labels_fixed[labels == 3] = 1
-    labels_fixed[labels == 4] = 2
-    labels = labels_fixed
-
-    return labels
-
-
-def soft_threshold_coefficients(coefficients, threshold):
-    if isinstance(threshold, float):
-        clean_coefficients = np.where(
-            np.abs(coefficients) > threshold, np.abs(coefficients) - threshold, 0
-        )
-        clean_coefficients = clean_coefficients * np.sign(coefficients)
-    else:
-        clean_coefficients = np.zeros(coefficients.shape)
-        for i, t in enumerate(threshold):
-            clean_coefficients[:, :, i] = np.where(
-                np.abs(coefficients[:, :, i]) > t, np.abs(coefficients[:, :, i]) - t, 0
-            )
-            clean_coefficients[:, :, i] = clean_coefficients[:, :, i] * np.sign(
-                coefficients[:, :, i]
-            )
-    return clean_coefficients
-
-
-def universal_threshold(coefficients):
-    """Threshold from VisuShrink"""
-    M = coefficients.shape[0] * coefficients.shape[1]
-    sigmas = []
-    for i in range(coefficients.shape[2]):
-        sigmas.append((np.median(np.abs(coefficients[:, :, i])) / 0.6745) ** 2)
-    sigmas = np.array(sigmas)
-    thresholds = np.sqrt(2 * np.log(M)) * sigmas
-    return thresholds
-
-
-def sure_threshold(coefficients, thresholds=None):
-    if thresholds is None:
-        thresholds = np.logspace(1e-6, 1e6, 10)
-
-    sigma = ...
-    sure_list = []
-    erisk_list = []
-    dof_list = []
-    for t in thresholds:
-        clean_coefficients = soft_threshold_coefficients(coefficients, t)
-        erisk_list.append(np.sum((clean_coefficients - coefficients) ** 2))
-        dof_list.append(np.sum(np.abs(clean_coefficients) > 0))
-
-    sure_list = np.array(erisk_list) + np.array(dof_list) * sigma**2
-    best_threshold = thresholds[np.argmin(sure_list)]
-    return best_threshold
-
-
 def get_scaled_coefficients(coefficients, scales=None):
     # scale coefficients
     n = coefficients.shape[0]
@@ -140,6 +29,10 @@ def get_scaled_coefficients(coefficients, scales=None):
         for i in range(n_s):
             scaled_coefficients[:, i] = scaled_coefficients[:, i] * scales[i]
 
+    # if is all zero, return
+    if scaled_coefficients.max() == 0:
+        return scaled_coefficients.reshape((n, n_t, n_s))
+    
     for i in range(n_s):
         scale_max = np.nanmax(scaled_coefficients[:, i])
         scaled_coefficients[:, i] = np.log(scaled_coefficients[:, i] + 1) / np.log(
@@ -303,62 +196,3 @@ def wavelet_power_spectrum(coefficients):
     power = np.abs(coefficients) ** 2
     power = power.sum(axis=1)
     return power
-
-
-def dominant_label_time(labels, signal=None):
-    """
-    Compute the frequency of labels in each timestep and normalize them.
-    After, identify the most frequent label in each timestep.
-    If the signal is provided, identify the sign of the signal of the most frequent label in each timestep.
-
-    Inputs:
-        labels - [n, n_t] array with labels
-
-    Outputs:
-        dominant - [n_t] array with dominant labels
-        signal_dominant - [n_t] array with dominant labels signal
-    """
-    unique_labels = np.unique(labels.reshape(-1))
-    max_count = []
-    for l in unique_labels:
-        max_count.append(np.sum(labels == l, axis=0).max())
-
-    dominant = np.zeros(labels.shape[1])
-    for t in range(labels.shape[1]):
-        count = []
-        for j, l in enumerate(unique_labels):
-            count.append(np.sum(labels[:, t] == l))
-        count = np.array(count)
-        dominant[t] = unique_labels[
-            np.argmax([c / m for (c, m) in zip(count, max_count)])
-        ]
-
-    if not signal is None:
-        signal_dominant = np.zeros(signal.shape[1])
-        for t in range(signal.shape[1]):
-            dominant_label = dominant[t]
-            filtered_signal = signal[labels[:, t] == dominant_label, t]
-            positive_count = np.sum(filtered_signal > 0)
-            negative_count = np.sum(filtered_signal < 0)
-            if positive_count > negative_count:
-                signal_dominant[t] = 1
-            else:
-                signal_dominant[t] = -1
-
-        return dominant, signal_dominant
-
-    return dominant
-
-
-def haversine(lon1, lat1, lon2, lat2):
-    lon1, lat1, lon2, lat2 = (
-        np.radians(lon1),
-        np.radians(lat1),
-        np.radians(lon2),
-        np.radians(lat2),
-    )
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = np.sin(dlat / 2.0) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2.0) ** 2
-    return 6367 * 2 * np.arcsin(np.sqrt(a))
-
